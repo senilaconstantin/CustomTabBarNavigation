8f95136824c86e2b7158663900f67c090b7964e7
senilaconstantin <constantin.senila@gmail.com>
2024-02-23 13:27:07 +0200

    First commit

---
+++ CustomTabBarNavigation/CustomTabBarNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomTabBarNavigation
+//
+//  Created by Constantin Senila on 23.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomTabBarNavigation/CustomTabBarNavigation/CustomTabBarNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarNavigationApp.swift
+//  CustomTabBarNavigation
+//
+//  Created by Constantin Senila on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
