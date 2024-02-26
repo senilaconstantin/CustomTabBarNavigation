720ea510666539f63e68bdcb009da0bfb0b405a5
senilaconstantin <constantin.senila@gmail.com>
2024-02-26 13:13:18 +0200

    added page animations

--- CustomTabBarNavigation/Pods/Target Support Files/Pods-CustomTabBarNavigation-CustomTabBarNavigationUITests/Pods-CustomTabBarNavigation-CustomTabBarNavigationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomTabBarNavigation_CustomTabBarNavigationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomTabBarNavigation_CustomTabBarNavigationUITests
-@end
--- CustomTabBarNavigation/Pods/Target Support Files/Pods-CustomTabBarNavigationTests/Pods-CustomTabBarNavigationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomTabBarNavigationTests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomTabBarNavigationTests
-@end
